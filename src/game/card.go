95c42b4c826ed088fe59f63a513be7dcbc13f657
simpdo <simpdo@qq.com>
2016-03-03 19:18:02 +0800

    2016-3-3 19:17:58

--- src/game/card.go
+++ src/game/card.go
@@ -3,30 +3,94 @@ package main
 import ()
 
 const (
-	Spade   = iota //黑桃
-	Heart          //红桃
-	Club           //梅花
-	Diamond        //方块
+	Spade   = iota + 1 //黑桃
+	Heart              //红桃
+	Club               //梅花
+	Diamond            //方块
+
+	CardBase   = 13
+	BlackJoker = 53
+	RedJoker   = 54
+
+	//牌型
+	CARD_ILLEGAL = iota
+
+	CARD_SINGLE
+	CARD_PAIR
+	CARD_ROCKET
+	CARD_THREE
+	CARD_THREE_WITH_SINGLE
+	CARD_THREE_WITH_PAIR
+	CARD_BOMB
+	CARD_BOMB_WITH_SINGLE
+	CARD_BOMB_WITH_PAIR
+	CARD_STRAIGHT
+	CARD_STRAIGHT_PAIR
+
+	CARD_PLANE
+	CARD_PLANE_WITH_SINGLE
+	CARD_PLANE_WITH_PAIR
 )
 
 type GameCard struct {
-	Value int8
-	Shape int
+	Point int8
+	Shape int8
 }
 
 type GameCardSet []GameCard
 
-func (arr GameCardSet) Len() int {
-	return len(arr)
+func (cards GameCardSet) Len() int {
+	return len(cards)
 }
 
-func (arr GameCardSet) Swap(i, j int) {
-	arr[i], arr[j] = arr[j], arr[i]
+func (cards GameCardSet) Swap(i, j int) {
+	cards[i], cards[j] = cards[j], cards[i]
 }
 
-func (arr GameCardSet) Less(i, j int) bool {
-	if arr[i].Value == arr[j].Value {
-		return arr[i].Shape < arr[j].Shape
+func (cards GameCardSet) Less(i, j int) bool {
+	first := cards[i].Point
+	if cards[i].Point == 1 || cards[i].Point == 2 {
+		first = first + CardBase
 	}
-	return arr[i].Value < arr[j].Value
+
+	second := cards[j].Point
+	if cards[j].Point == 1 || cards[j].Point == 2 {
+		second = second + CardBase
+	}
+
+	if cards[i].Point == cards[j].Point {
+		return cards[i].Shape < cards[j].Shape
+	}
+
+	return first < second
+}
+
+func NewCard(Value int8) *GameCard {
+	if Value < 1 || Value > RedJoker {
+		return nil
+	}
+
+	if Value == BlackJoker || Value == RedJoker {
+		return &GameCard{
+			Point: Value,
+			Shape: 0,
+		}
+	}
+
+	return &GameCard{
+		Point: int8(Value % CardBase),
+		Shape: int8(Value / CardBase),
+	}
+}
+
+func (this *GameCard) Value() int8 {
+	if this.Point == BlackJoker || this.Point == RedJoker {
+		return this.Point
+	}
+
+	return int8(CardBase*(this.Shape-1) + CardBase)
+}
+
+func (this *GameCard) Type() int {
+	return 0
 }
